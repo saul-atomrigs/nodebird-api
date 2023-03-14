85371d442f0da5dfcd1b87aa8e73ff2c43fcb62c
Saul Lee <solleedata@gmail.com>
2023-03-14 12:48:43 +0900

    API 사용량 제한 구현하기 (미들웨어 추가)

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
 
 exports.verifyToken = (req, res, next) => {
     try {
@@ -18,3 +19,24 @@ exports.verifyToken = (req, res, next) => {
     })
     }
 }
+
+// 라우터에 사용량 제한을 설정하는 미들웨어:
+exports.apiLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1분
+    max: 1,
+    handler(req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode, // 기본값 429
+            message: '1분에 한 번만 요청할 수 있습니다',
+        });
+    }
+})
+
+// 사용하면 안 되는 라우터에 붙이는 미들웨어:
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요',
+    });
+}
+
